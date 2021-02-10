9fe052c574bb4814efb0be3ad3419ba8faf0e743
Eli Reiners <goelyukim@gmail.com>
2021-02-10 13:42:35 -0500

    Use node fetch Remove unnecessary code

--- src/formatter-router/formatter.js
+++ src/formatter-router/formatter.js
@@ -1,7 +1,8 @@
 const path = require('path');
-const express = require('express')
-const xss = require('xss')
+const express = require('express');
+const xss = require('xss');
 const fs = require('fs');
+const fetch = require('node-fetch');
 //const debug = require('debug')('express:view')
 const FoldersService = require('./formatter-service');
 
@@ -17,97 +18,27 @@ formatRouter
     .route('/')
     .get((req, res, next) => {
 
-        const content = 'Some content!!!!'
+        // const content = 'Some content!!!!'
 
-        try {
-          const data = fs.appendFile('/Users/eliriner/24hours/formate-images/src/formatter-router/test.txt', content, err => {})
-          //file written successfully
-        } catch (err) {
-          console.error(err)
-        }
-            res.status(200).end()
-   
+        const url = 'http://assets.aosmith.com.s3.amazonaws.com/damrootDEV/Original\10001\State_ProLine_Lowboy_Side_Connect_and_Top_T_&_P_Electric_Water_Heater.tif'; // link to file you want to download
+        const path = '/Users/eliriner/24hours/formate-images/src/local/images.png' // where to save a file
 
-    })
-    .post(jsonParser, (req, res, next) => {
-        const { folder_name } = req.body;
-        const newFolder = { folder_name };
+        fetch(url)
+            .then((res) => {
 
-        if (!folder_name) {
-            return res.status(400).json({
-                error: { message: `Missing 'folder_name' in request body` }
-            })
-        }
-
-        FoldersService.insertFolder(
-            req.app.get('db'),
-            newFolder
-        )
-
-            .then(folder => {
-                res
-                    .status(201)
-                    .location(path.posix.join(req.originalUrl, `/${folder.id}`))
-                    .json(serialize(folder))
+                fs.writeFile(path, res, (err) => {
+                    if (err) {
+                      console.error(err)
+                      return
+                    }
+                    //file written successfully
+                  })
             })
             .catch(next)
+        res.status(200).end()
+
+
     })
 
-formatRouter
-    .route('/folder/:folder_id')
-    .all((req, res, next) => {
-        FoldersService.getById(
-            req.app.get('db'),
-            req.params.folder_id
-        )
-            .then(folder => {
-                if (!folder) {
-                    return res.status(404).json({
-                        error: { message: `Folder does not exist` }
-                    })
-                }
-                res.folder = folder;
-                next()
-            })
-            .catch(next)
-
-    })
-    .get((req, res, next) => {
-        res.json({
-            id: res.folder.id,
-            folder_name: xss(res.folder_name), // sanitize title
-        })
-    })
-    .delete(jsonParser, (req, res, next) => {
-        FoldersService.deleteFolder(req.app.get('db'),
-            req.params.folder_id)
-            .then(numRowsAffected => {
-                res.status(204).end()
-            })
-            .catch(next)
-    })
-    .patch(jsonParser, (req, res, next) => {
-        const { folder_name } = req.body;
-        const folderToUpdate = { folder_name }
-
-
-        if (!folder_name) {
-            return res.status(400).json({
-                error: { message: `Request body must contain a folder_name ` }
-            })
-        }
-
-        FoldersService.updateFolder(
-            req.app.get('db'),
-            req.params.folder_id,
-            folderToUpdate
-        )
-
-            .then(numRowsAffected => {
-                res.status(204).end()
-            })
-
-            .catch(next)
-    })
 
 module.exports = formatRouter
